493b3ad14ea9d09020da111fcf13916ce5057b83
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2019-10-24 11:11:54 +0200

    torsdag

--- src/EquationSolver.java
+++ src/EquationSolver.java
@@ -4,13 +4,31 @@ public class Ekvation {
     public static void main(String[] args) {
 
         while(true) {
+            int grad = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad"));
+        } if (grad =0) {
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange a"));
+            JOptionPane.showMessageDialog(a);
+
+        } else if (grad =1) {
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange a"));
+            double b = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange b"));
+            JOptionPane.showMessageDialog(-a/b);
+
+        } else if (grad =2) {
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange a"));
+            double b = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange b"));
+            double c = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange c"));
+            JOptionPane.showMessageDialog(-(b/2a) +((b/(2a)^2) - b);
+
+        } else if (grad =3) {
+            double a = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange a"));
+            double b = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange b"));
+            double c = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange c"));
+            double d = Double.parseDouble(JOptionPane.showInputDialog(null, "Ange d"));
+            JOptionPane.showMessageDialog();
 
         }
 
-        int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange a"));
-        int potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad"));
-        int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange b"));
-        int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange c"));
 
     }
 }
