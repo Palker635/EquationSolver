e43f68fd9078e37d786d2bea545415a6143e26ca
isac.hansson <Isac.hansson@elev.ga.ntig.se>
2019-10-24 10:45:22 +0200

    Initial commit

---
+++ src/EquationSolver.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Ekvation {
+    public static void main(String[] args) {
+
+        while(true) {
+
+        }
+
+        int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange a"));
+        int potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange b"));
+        int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange c"));
+
+    }
+}
